fb51545acfe65b10491d7419c0f1c8c2345eb36d
Take IT <admin@takeit.vn>
2023-04-27 13:37:40 +0700

    update gem file and seed

--- Gemfile
+++ Gemfile
@@ -40,14 +40,18 @@ gem 'pagy'
 gem 'ransack'
 gem 'activerecord-import'
 gem 'pundit'
+gem 'faker'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
+
 end
 
 group :development do
   # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
   # gem "spring"
+  gem 'bullet'
+  gem "awesome_print"
 end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,30 +13,30 @@ User.create(
   password: '123123A@',
   role: 'sys'
 )
-#
-# users = User.all
-# card_collections = []
-# users.each do |user|
-#   (1..50).each do |i|
-#     card_collections << CardCollection.new(
-#       name: Faker::Educator.subject,
-#       description: Faker::Educator.course_name,
-#       user_id: user.id
-#     )
-#   end
-# end
-# CardCollection.import card_collections
-#
-# cards = []
-# card_collections = CardCollection.all
-# card_collections.each do |card_collection|
-#   (1..15).each do |i|
-#     cards << Card.new(
-#       question: Faker::JapaneseMedia::Naruto.character,
-#       answer: Faker::JapaneseMedia::Naruto.village,
-#       card_collection_id: card_collection.id,
-#       user_id: card_collection&.user&.id
-#     )
-#   end
-# end
-# Card.import cards
+
+users = User.all
+card_collections = []
+users.each do |user|
+  (1..50).each do |i|
+    card_collections << CardCollection.new(
+      name: Faker::Educator.subject,
+      description: Faker::Educator.course_name,
+      user_id: user.id
+    )
+  end
+end
+CardCollection.import card_collections
+
+cards = []
+card_collections = CardCollection.all
+card_collections.each do |card_collection|
+  (1..15).each do |i|
+    cards << Card.new(
+      question: Faker::JapaneseMedia::Naruto.character,
+      answer: Faker::JapaneseMedia::Naruto.village,
+      card_collection_id: card_collection.id,
+      user_id: card_collection&.user&.id
+    )
+  end
+end
+Card.import cards
