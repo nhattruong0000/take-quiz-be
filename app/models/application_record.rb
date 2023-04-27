a064185a96994d8faee9411449f52c111d99bde6
Take IT <admin@takeit.vn>
2023-04-27 14:01:25 +0700

    update ransackable

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,6 +1,10 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 
+  def self.ransackable_attributes(auth_object = nil)
+    super
+  end
+
   def self.found_data_message
     "The data has been retrieved successfully"
   end
--- app/models/card_collection.rb
+++ app/models/card_collection.rb
@@ -2,4 +2,9 @@ class CardCollection < ApplicationRecord
   has_many :cards, class_name: 'Card', dependent: :restrict_with_error, foreign_key: :card_collection_id, primary_key: :id
 
   belongs_to :user, class_name: 'User', foreign_key: :user_id, primary_key: :id, optional: true
+
+  # def self.ransackable_attributes(auth_object = nil)
+  #   ["created_at", "description", "id", "name", "status", "updated_at", "user_id"]
+  # end
+
 end
