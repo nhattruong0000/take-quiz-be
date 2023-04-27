cf56f2d835f686fc26e2776b96da296e2a6e7865
Take IT <admin@takeit.vn>
2023-04-27 13:41:23 +0700

    update router

--- config/routes.rb
+++ config/routes.rb
@@ -12,14 +12,14 @@ Rails.application.routes.draw do
   post 'authentication/change_password_with_token', to: 'authentication#change_password_with_token'
   get 'authentication/auth_verify', to: 'authentication#auth_verify'
 
-  # namespace :api, as: '' do
-  #   namespace :v1, as: '' do
-  #     namespace :workplace, as: '' do
-  #       get 'dashboard/', to: 'dashboard#index'
-  #       resources :card_collection, only: [:index, :show, :create, :update, :destroy]
-  #       get 'card_collection/:id/cards', to: 'card_collection#cards'
-  #     end
-  #   end
-  # end
+  namespace :api, as: '' do
+    namespace :v1, as: '' do
+      namespace :workplace, as: '' do
+        get 'dashboard/', to: 'dashboard#index'
+        resources :card_collection, only: [:index, :show, :create, :update, :destroy]
+        get 'card_collection/:id/cards', to: 'card_collection#cards'
+      end
+    end
+  end
 
 end
