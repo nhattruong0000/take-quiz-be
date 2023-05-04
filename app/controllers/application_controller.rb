86b2b98fde0d6c861fe3067a02073cbaa4fbbc06
Take IT <admin@takeit.vn>
2023-05-04 10:28:01 +0700

    update how to use secret key base

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,7 +36,7 @@ class ApplicationController < ActionController::API
       p Rails.application.credentials.jwt_auth.iss
       p Rails.application.credentials.jwt_auth.aud
       decoded_token = JWT.decode token,
-                                 Rails.application.secrets.secret_key_base,
+                                 Rails.application.credentials.secret_key_base,
                                  true,
                                  { verify_iss: true,
                                    iss: Rails.application.credentials.jwt_auth.iss,
